375cc48a35bd122bacc6480a7b2e4b810a6aaf06
Stas Onatskiy <code@onatsky.ru>
2018-06-26 16:16:47 +0300

    add plugin

---
+++ plugins/sonatskiy-1/test/example.js
@@ -0,0 +1,10 @@
+const bar = 'foo'
+const dar = 5
+const bol = true, fol = 'da'
+const mull = null
+const bjj = {}
+const arr = []
+const fuu = () => {}
+const muu = function () {}
+
+let not = false
